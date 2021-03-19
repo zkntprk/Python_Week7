c1318e844ff4290c22b620af80e0ed196740de62
özkan <58474594+zkntprk@users.noreply.github.com>
2021-03-19 15:40:40 +0200

    Update Week -7 - Q-1.py

--- Week -7 - Q-1.py
+++ Week -7 - Q-1.py
@@ -2,7 +2,7 @@
 
 #Copyright (c) ${HiddenIDFinder.2021} ${Ozkan TOPRAK}
 
-#Created by InfotecAcademy
+#Created by InfotechAcademy
 
 #THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 #IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
@@ -49,7 +49,7 @@ findhiddenID()
 
 #Copyright (c) ${HiddenIDFinder.2021} ${Ozkan TOPRAK}
 
-#Created by InfotecAcademy
+#Created by InfotechAcademy
 
 #THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 #IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
