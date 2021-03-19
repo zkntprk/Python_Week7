b9fa470c98a49de8acff63dfddcb7022a9440a23
özkan <58474594+zkntprk@users.noreply.github.com>
2021-03-19 15:27:11 +0200

    Add files via upload

---
+++ Week -7 - Q-3.py
@@ -0,0 +1,28 @@
+# #
+# Write a program that list according to email addresses without email domains in a text.
+#
+# Example:
+#
+# Input:
+#
+# The advencements in biomarine studies franky@google.com with the investments necessary and Davos sinatra123@yahoo.com.
+# Then New Yorker article on wind farms...
+#
+# Output :
+#
+# franky
+#
+# sinatra123
+
+import re
+
+with open('mailtext.txt', 'r', encoding="utf-8") as file:
+    text = file.read()
+    # pattern = r'\S+@'
+    pattern = r'[a-z|A-Z|0-9]+[\._]?[a-z|A-Z|0-9]+[@]'
+    lst = re.findall(pattern, text)
+    new_lst = []
+
+    for i in range(len(lst)):
+        new_lst.append(lst[i][:-1])
+        print(new_lst[i])
